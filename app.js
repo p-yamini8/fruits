6fc104719df86779f18e331447b4f6c65f2ca96d
p-yamini8 <yaminifreez1@gmail.com>
2024-12-24 14:54:32 +0530

    request-response

---
+++ app.js
@@ -0,0 +1,36 @@
+const http=require('http');
+const server=http.createServer((req,res)=>{
+    console.log("server is created");
+    res.setHeader('Content-Type','text/html');
+    if(req.url=='/'){
+        res.statusCode=200;
+        res.end("<h1>Hello World</h1>")
+    }
+    else if(req.url=='/pizza'){
+        res.statusCode=200;
+        res.end("<h1> this is your pizza</h1>")
+    }
+    else if(req.url=='/home')
+    {res.statusCode=200;
+        res.end('<h1>Welcome home</h1>');
+
+    }
+    else if(req.url=='/about')
+        {res.statusCode=200;
+            res.end("<h1>Welcome to About Us</h1>");
+    
+        }
+        else if(req.url=='/node')
+            {res.statusCode=200;
+                res.end('<h1>Welcome to my Node Js project</h1>');
+        
+            }
+            else{
+                res.statusCode=404;
+                res.end('<h1>Page Not Found</h1>');
+            }
+})
+let port=4000;
+server.listen(port,()=>{
+    console.log("server is runing");
+});
