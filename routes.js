9d2246306ee7ed7cc85ab9612af9f4994af275f8
p-yamini8 <yaminifreez1@gmail.com>
2024-12-28 12:25:03 +0530

    using routes

--- routes.js
+++ routes.js
@@ -1,50 +1,47 @@
 const fs=require('fs');
-const requestHandler=(req,res)=>{
-    const url=req.url;
-    const method=req.method;
+const handler=(req,res)=>{
     if(req.url=="/")
-    {
-        res.setHeader('content-Type','text/html');
-        res.end(`
-            <form action='/message' method='POST'>
-            <label>Name:</label>
-            <input type="text" name='username'></input>
-            <button type='submit'>Add</button>
-            </form>
-    
-            `)
-    }
-    if(req.url=="/message")
-    {
-        res.setHeader('content-Type','text/html');
-       let datachunks=[];
-       req.on('data',(chunks)=>{
-        console.log(chunks)
-        datachunks.push(chunks);
-       })
-       req.on('end',()=>{
-        let combinedbuffer=Buffer.concat(datachunks);
-        console.log(combinedbuffer);
-        let formdata=combinedbuffer.toString();
-        console.log(formdata);
-        const formvalues=formdata.split('=')[1];
-        fs.writeFile('formvalues.txt',formvalues,(err)=>{
-    res.statusCode=302;
-    res.setHeader('Location','/');
-    res.end();
-        })
-       });
-    }
-    else{
-        if(req.url=='/read'){
-    fs.readFile('formvalues.txt',(err,data)=>{
-        console.log(data.toString());
-        res.end(`
-            <h1>${data.toString()}</h1>`)
-    })
+        {res.setHeader("content-Type",'text/html')
+            fs.readFile('form_data.txt',(err,data)=>{
+               const saveddata=data.toString();
+               res.end(`
+                <h1>${saveddata}</h1>
+                <form action='/message' method='POST'>
+                  <label>Name:</label>
+                  <input type="text" name="username"></input>
+                  <button type='submit'>Add</button>
+                </form>`)
+            })
+           
         }
+        else{
+            if(req.url=="/message"){
+                let a=[]
+                    req.on('data',(chunks)=>{
+            a.push(chunks);
+                    })
+                    req.on('end',()=>{
+                       const combinedbuffer= Buffer.concat(a);
+                      let b= combinedbuffer.toString();
+                      const form_data=b.split("=")[1];
+                    //  console.log(form_data);
+                      fs.writeFile("form_data.txt",form_data,(err)=>{
+                        res.statusCode=302;
+                        res.setHeader('Location',"/")
+                        res.end();
+                      })
+                    });
+                }
+                else{
+                    if(req.url=="/read")
+                    {
+                        fs.readFile("form_data.txt",(err,data)=>{
+                        //  console.log(data.toString());
+                         res.end(`<h1>${data.toString()}</h1>`);
+                        })
+                    }
+                }
+            }
     }
-}
-
-module.exports.handler=requestHandler;
-//module.exports.testFunction=anotherFunction;
+   
+module.exports=handler;
